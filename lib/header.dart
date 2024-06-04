332ab50ef001b9efbd605446c8f9864aead3bf5e
Ibrahim Imam <ibrhmmm552@gmail.com>
2024-06-04 19:44:20 +0700

    first

---
+++ lib/header.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key, required this.height, this.child});
+
+  final double height;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/bg.png"), fit: BoxFit.cover),
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(12),
+          bottomRight: Radius.circular(12),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 52),
+        child: Center(child: child),
+      ),
+    );
+  }
+}
